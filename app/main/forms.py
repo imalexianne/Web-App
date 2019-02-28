e0c2e098d3627228d3325901d85787be4d6c4f74
imalexianne <imanirax@yahoo.fr>
2019-02-28 10:16:59 +0200

    add forms

---
+++ app/main/forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+
+# class ReviewForm(FlaskForm):
+
+#     title = StringField('Review title',validators=[Required()])
+#     review = TextAreaField('Movie review', validators=[Required()])
+#     submit = SubmitField('Submit')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    category = StringField('Pitch category',validators=[Required()])
+    description = TextAreaField('user pitch')
+    submit = SubmitField('Submit')
+
+
+# class PitchForm(FlaskForm):
+#     category = StringField('Pitch category',validators=[Required()])
+#     description = TextAreaField('user pitch')
+#     submit = SubmitField('Submit')
