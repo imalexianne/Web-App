b249b7751191c3a7167a5fe1e1afb360669d1d58
imalexianne <imanirax@yahoo.fr>
2019-02-28 12:02:26 +0200

    export

---
+++ start.sh
@@ -0,0 +1,7 @@
+# export MOVIE_API_KEY='7854ce99b646526f11604daf32dc1202'
+
+# python3.6 manage.py server
+export SECRET_KEY='0788504294ma0788896659pa'
+export MAIL_USERNAME=imalexianne
+export MAIL_PASSWORD=wecode/123
+python3.6 manage.py server
