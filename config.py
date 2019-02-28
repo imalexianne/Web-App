e61700239a8cf694b77a390d2372df8c9bac38a9
imalexianne <imanirax@yahoo.fr>
2019-02-28 11:59:10 +0200

    add classes for configurations

--- config.py
+++ config.py
@@ -1,7 +1,44 @@
+import os
+
 class Config:
 
     # MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:alexianne@localhost/webapp'
+   
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    # email configurations
+    MAIL_SERVER ='smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREF = 'Webapp'
+    SENDER_EMAIL  = 'imalexianne@gmail.com'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:alexianne@localhost/webapp'
 
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:alexianne@localhost/webapp'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
